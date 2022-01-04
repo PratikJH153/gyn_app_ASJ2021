9d674c7c988b9b9354a1182ac7fd4eb2be76715b
AKAMasterMind404 <59548859+AKAMasterMind404@users.noreply.github.com>
2022-01-04 10:04:20 +0530

    Revert "customizations"
    This reverts commit 58cd90732595c8584d44dddb3462c5193b5d213e.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.home, R.id.favorites, R.id.backup
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
